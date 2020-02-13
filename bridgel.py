cec46ec8c0b130f3f5cc79d4090baf590a36c525
aashishogale <aashishogale9@gmail.com>
2020-02-13 19:26:29 +0530

    added bltools

--- bridgel.py
+++ bridgel.py
@@ -2,43 +2,57 @@ import click
 from github import Github
 from github import InputGitTreeElement
 import os
-import mysql.connector
 import requests
 import json
 from git import Repo
-from config import Config
+import jwt
+from os import path
+import uuid            
+
+
 
 
 @click.command()
 @click.option('--get_question', default=None)
 @click.option('--enter_solution', default=None)
 def add_to_git(get_question, enter_solution):
-    if get_question:       
-        mydb = mysql.connector.connect(**Config.SQL_CONFIG)
-        mycursor = mydb.cursor()
-        username = click.prompt('Please enter username', type=str)
-        password = click.prompt('Please enter a password', type=str, hide_input=True)
-        result = requests.post('http://127.0.0.1:8000/getAuthorization', data = json.dumps({'username': username, "password":password}))
-        result = json.loads(result.content)
+
+    if get_question: 
+        if path.exists(".bltools"):
+            with open(".bltools", "r+") as f:
+                token =  f.read()
+                user_dict = jwt.decode(token, "secret", algorithms=['HS256'])
+                if not user_dict.get("mac_id") == hex(uuid.getnode()):
+                    exit()
+        else:
+            username = click.prompt('Please enter username', type=str)
+            password = click.prompt('Please enter a password', type=str, hide_input=True)
+            user_dict = {"username": username, "password":password}
+            with open(".bltools", "w+") as f:
+                encoded_jwt = jwt.encode({'username': username, "password":password, "mac_id":hex(uuid.getnode()) 
+}, "secret", algorithm='HS256')
+                f.write(encoded_jwt.decode("utf-8"))
+        result = requests.post('http://127.0.0.1:8000/getAuthorization', data = json.dumps(user_dict))
+        result = json.loads(result.content)     
         filepath = os.path
-        collaborator = Github(username, password)
+        collaborator = Github(user_dict.get("username"), user_dict.get("password"))
         collaborator_user = collaborator.get_user()       
         if not result.get("token"):
             exit()
         g = Github(result.get("token"))
         user = g.get_user()
         try:
-            repo = user.create_repo(username+"_"+get_question)
+            repo = user.create_repo(user_dict.get("username")+"_"+get_question)
         except Exception as e:
             pass
         try:
             repo.create_file("README.md", "readme", "readme", branch="master")
             repo.create_file("{}.py".format(get_question), get_question, get_question, branch="master")
-            repo.add_to_collaborators(username, permission="push")
+            repo.add_to_collaborators(user_dict.get("username"), permission="push")
         except Exception as e:
             pass
         try:
-            repo = Repo.clone_from("https://github.com/aashishogale/{}.git".format(username+"_"+get_question),"/home/admin2/Documents/{}".format(username+"_"+get_question), branch='master')
+            repo = Repo.clone_from("https://github.com/aashishogale/{}.git".format(user_dict.get("username")+"_"+get_question),"/home/anurag/Documents/{}".format(user_dict.get("username")+"_"+get_question), branch='master')
         except Exception as e:
             pass
     if enter_solution:      
