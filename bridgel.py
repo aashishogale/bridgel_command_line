c7549aaa44692bdacd044ef01661b30558025d6b
aashishogale <aashishogale9@gmail.com>
2020-02-14 17:44:40 +0530

    added doc path

--- bridgel.py
+++ bridgel.py
@@ -13,7 +13,7 @@ import uuid
 
 
 @click.command()
-@click.option('--get_question', default=None)
+@click.option('--get_question', nargs=2, default=None)
 @click.option('--enter_solution', default=None)
 def add_to_git(get_question, enter_solution):
 
@@ -42,100 +42,45 @@ def add_to_git(get_question, enter_solution):
         g = Github(result.get("token"))
         user = g.get_user()
         try:
-            repo = user.create_repo(user_dict.get("username")+"_"+get_question)
+            repo = user.create_repo(user_dict.get("username")+"_"+get_question[0])
         except Exception as e:
             pass
         try:
             repo.create_file("README.md", "readme", "readme", branch="master")
-            repo.create_file("{}.py".format(get_question), get_question, get_question, branch="master")
+            repo.create_file("{}.py".format(get_question[0]), get_question[0], get_question[0], branch="master")
             repo.add_to_collaborators(user_dict.get("username"), permission="push")
         except Exception as e:
             pass
         try:
-            repo = Repo.clone_from("https://github.com/aashishogale/{}.git".format(user_dict.get("username")+"_"+get_question),"/home/anurag/Documents/{}".format(user_dict.get("username")+"_"+get_question), branch='master')
+            repo = Repo.clone_from("https://github.com/{}/{}.git".format(result.get("owner"),user_dict.get("username")+"_"+get_question[0]),"{}/{}".format(get_question[1], user_dict.get("username")+"_"+get_question[0]), branch='master')
+
         except Exception as e:
             pass
     if enter_solution:      
-        username = click.prompt('Please enter username', type=str)
-        password = click.prompt('Please enter a password', type=str, hide_input=True)
-        result = requests.post('http://127.0.0.1:8000/getAuthorization', data = json.dumps({'username': username, "password":password}))
+        with open(".bltools", "r+") as f:
+            token =  f.read()
+            user_dict = jwt.decode(token, "secret", algorithms=['HS256'])
+            if not user_dict.get("mac_id") == hex(uuid.getnode()):
+                exit()
+        result = requests.post('http://127.0.0.1:8000/getAuthorization', data = json.dumps(user_dict))
         result = json.loads(result.content)
         if not result.get("token"):
             exit()
-        repo = Repo("/home/admin2/Documents/{}".format(username+"_"+enter_solution))
-        print (repo.working_dir)
-        new_file_path = os.path.join(repo.working_tree_dir, "{}.py".format(enter_solution))
+        repo = Repo("/".join(enter_solution.split("/")[:-1]))
+        new_file_path = os.path.join(repo.working_tree_dir, "{}".format(enter_solution))
         repo.index.add([new_file_path])
         repo.index.commit("final_commit")
         repo.remotes.origin.push()
         g = Github(result.get("token"))
         user = g.get_user()
         try:
-            repo = user.get_repo(username+"_"+enter_solution)
+            repo = user.get_repo(enter_solution.split("/")[-2])
             sb = repo.get_branch("master")
             repo.create_git_ref(ref='refs/heads/Final-Release', sha=sb.commit.sha)
-
             sha = repo.get_branch("Final-Release").commit.sha
             tag = repo.create_git_tag(tag="v-1.0", message="tag message", object=sha, type="commit") 
             ref = "refs/tags/"+ tag.tag
             repo.create_git_ref(ref, tag.sha)
         except Exception as e:
             pass
-        result = requests.post('http://127.0.0.1:8000/saveTagLink', data = json.dumps({'username': username, "repository_name":enter_solution}))
-
-
-        
-
-
-
-
-    
-
-
- 
-
-
-
-
-
-
-     
-
-        
-
-
-
-        
-
-    
-
-
-
-
-
-
-
-
-
-   
-        
-
-        
-
-        
-
-
-   
-    
-
-        # repo.create_file(os.path.abspath(add[1].strip("/")), "init commit", "added")         
-        # print(os.path.abspath(add)) 
-
-
-
-
-
-
-    
-    
-
+        result = requests.post('http://127.0.0.1:8000/saveTagLink', data = json.dumps({'username': user_dict.get("username"), "repository_name":enter_solution.split("/")[-1]}))
